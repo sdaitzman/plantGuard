0595022896c3e9cfea7471732bacb15968646d24
sdaitzman <samuel@daitzman.com>
2015-05-09 06:31:53 -0400

    sets up gulp

--- plantGuard-data/gulpfile.js
+++ plantGuard-data/gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task(function() {
-	
+gulp.task('sass', function() {
+	console.log("wasdasd");
+	gulp.src('.scss/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
 });
+
+gulp.task('default', ['sass']);
